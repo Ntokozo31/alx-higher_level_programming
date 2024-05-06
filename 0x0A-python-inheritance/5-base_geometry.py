cef5097fd47506ed9401dce6ad0e42d362ee52d0
@Ntokozo31 <dubentokozo45@ahoo.com>
2024-05-06 03:42:28 -0700

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    """
+    A base class representing geometry.
+
+    This class serves as a base for defining geometric shapes and operations.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass

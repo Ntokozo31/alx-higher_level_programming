bb26bf98ffdfedf1155baf91e4ae6968a1cea10a
Ntokozo31 <dubentokozo45@yahoo.com>
2024-06-19 03:22:29 -0700

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 
-// check if atleast two arguments are passed
-if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
-  // Print the two arguments in a specified format
-  console.log(`${process.argv[2]} is ${process.argv[3]}`);
+// first user provided argument
+const arg1 = process.argv[2];
+// Second user provided argument
+const arg2 = process.argv[3];
+
+if (!arg1 && !arg2) {
+  // If both arguments are false (i.e undefined)
+  console.log('undefined is undefined');
+} else if (!arg2) {
+  // If argument 1 is true but argument 2 is false
+  console.log(`${arg1} is undefined`);
 } else {
-  // Print an error massege less then two arguments is passed
-  console.log('please provide at least two arguments');
+  // If both arguments are true
+  console.log(`${arg1} is ${arg2}`);
 }

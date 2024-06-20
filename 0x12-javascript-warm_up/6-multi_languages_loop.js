5dbb46d8d446a3094bb21b65088dac4154ab1024
Ntokozo31 <dubentokozo45@yahoo.com>
2024-06-20 08:32:40 -0700

    6-multi_languages_loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Define an array of strings
+const masseges = ['C is fun', 'Python is cool', 'JavaSricpt is amazing'];
+// Loop through the elements of the masseges array
+for (let i = 0; i < masseges.length; i++) {
+  // Print out each massege in the array
+  console.log(masseges[i]);
+}

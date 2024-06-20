cb04971144f6700843ea7a685112906e81249703
Ntokozo31 <dubentokozo45@yahoo.com>
2024-06-20 13:09:31 -0700

    103-object_fct.js

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12,
+  incr() {
+    this.value++;
+  }
+};
+console.log(myObject);
+
+// Increment the value and print the object
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);

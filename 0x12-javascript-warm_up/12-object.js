9326ef5467f8da7fa0108ab3ade39a7ad14c2ff6
Ntokozo31 <dubentokozo45@yahoo.com>
2024-06-20 12:26:18 -0700

    12-object.js

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+// Update the value property of myObjct
+myObject.value = 89;
+console.log(myObject);

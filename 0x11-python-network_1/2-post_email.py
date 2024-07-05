317e5ea112f98d74ebabc695f0b33fd8951ecf23
Ntokozo31 <dubentokozo45@yahoo.com>
2024-07-05 16:15:30 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,20 +3,21 @@
 # We are going to write a scrip that will take url and email
 # We are going to check if the script is run dicectly
 
+# we import neccesary modules here
+from urllib import request, parse
+import sys
+
+# We check if the code is run directly
 if __name__ == "__main__":
-    # We import neccesary modules
-    import urllib.request
-    import urllib.parse
-    import sys
 
     # Assign our argumens
-    argv = sys.argv
-    url = argv[1]
-    email = argv[2]
-    DATA = urllib.parse.urlencode({"email": email})
-    DATA = DATA.encode('ascii')
+    my_values = {'email': sys.argv[2]}
+    data = parse.urlencode(my_values)
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1], data)
 
     # We open our url with our parameter
-    with urllib.request.urlopen(url, DATA) as response:
+    with request.urlopen(req) as response:
+        my_body = response.read()
         # We print out
-        print(response.read().decode("utf-8"))
+        print(my_body.decode('utf-8'))

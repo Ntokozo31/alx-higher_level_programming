c33234a284df1ff1b7a233c3b9fbf2f5a0e0f2ac
Ntokozo31 <dubentokozo45@yahoo.com>
2024-07-05 23:19:08 -0700

    0-httn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
+
 # We are going to write a script that will facth a URL
 
 import urllib.request
 
-# We are chicking if the script is run directly or what
 if __name__ == "__main__":
-    # WE are going to creat a request object now
     req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    # Now it time to open our URL
     with urllib.request.urlopen(req) as response:
-        # Now let us read our response
         my_body = response.read()
-        # Let us first print the header
         print("Body response")
-        # Let us print the type of our response body
         print("\t - type: {}".format(type(my_body)))
-        # Let print the core content of response of the body
         print("\t - content: {}".format(my_body))
-        # Let print UFT-8 decoded content of the response of our body
         print("\t - utf-8 content: {}".format(my_body.decode("utf-8")))

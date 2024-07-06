7c645d50ffb4d14013a2863515904af987eccb6c
Ntokozo31 <dubentokozo45@yahoo.com>
2024-07-05 23:13:51 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
-# We are going to write a scrip that will take url and email
-# We are going to check if the script is run dicectly
+"""
+We are going to write a scrip that will take url and email
+sends a post request to the passed URL with the email as parameter
+and displays the body of the response decoded in utf-8
+"""
 
 from urllib import request, parse
 import sys
 
 if __name__ == "__main__":
 
-    # Assign our argumens
     my_values = {'email': sys.argv[2]}
     data = parse.urlencode(my_values)
     data = data.encode('ascii')
     req = request.Request(sys.argv[1], data)
 
-    # We open our url with our parameter
     with request.urlopen(req) as response:
         my_body = response.read()
-        # We print out
         print(my_body.decode('utf-8'))

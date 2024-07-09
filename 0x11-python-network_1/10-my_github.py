b67ae98120811e1b3a906681ab1b99505ba8ef1d
Ntokozo31 <dubentokozo45@yahoo.com>
2024-07-09 08:14:48 -0700

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-
 """
-We must take our Github credentials (username, password) and use Github API
-to display our id
+We are going GitHub credentials (username and password) and uses the GitHub API
+to display your id
 """
 
-if __name__== "__main__":
+if __name__ == '__main__':
     from requests import get
     from sys import argv
 
     username = argv[1]
     password = argv[2]
 
-    URL = 'https://api.github.com/user'
+    URL = "https://api.github.com/user"
     response = get(URL, auth=(username, password))
     json = response.json()
 

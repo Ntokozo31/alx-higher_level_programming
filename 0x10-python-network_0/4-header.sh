b2ae5dd378716ad1fda950e40de29884278bd5a6
Ntokozo31 <dubentokozo45@yahoo.com>
2024-07-09 08:40:16 -0700

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# Send a GET request to URL, display the body response
-
-# Check if a URL was provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Define the URL from the first argument
-URL="$1"
-
-# Send the GET request with the specified header
-curl -sH "X-HolbertonSchool-User-Id: 98" "$URL"
+# Sends a GET request to the URL, and displays the body of the response
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"

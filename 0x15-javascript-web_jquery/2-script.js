d57d7c2cb9bcee2768776c18eb3cd9044a726c24
Ntokozo31 <dubentokozo45@yahoo.com>
2024-08-18 01:24:37 -0700

    0x15-javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+	$('#header_red').on('click', () => {
+		$('#header_red').css('color', '#FF0000');
+	});
+});
---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+	$('.red').on('click', () => {
+		$('#header_red').add();
+	});
+});
